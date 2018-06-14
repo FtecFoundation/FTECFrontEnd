3999ef70213a6d354ccfc851803a75ca71e32d04
User <user@MacBook-Pro-User-3.local>
2018-06-14 18:52:25 +0300

    not working feature

--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -7,7 +7,7 @@ import {Component, ElementRef, Input, OnInit} from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  showPopup: boolean = true;
+  showPopup = true;
 
   constructor() {
   }
