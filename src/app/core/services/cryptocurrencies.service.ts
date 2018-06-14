32691ca51739b9bae9673ce705cf013673c1d15b
Julia <arikamedoshika@gmail.com>
2018-06-14 13:29:00 +0300

    logout feature fix

--- src/app/core/services/cryptocurrencies.service.ts
+++ src/app/core/services/cryptocurrencies.service.ts
@@ -5,12 +5,12 @@ import {HttpClient, HttpParams} from '@angular/common/http';
 @Injectable()
 export class CryptocurrenciesService {
 
-  getCryptocurrenciesUrl = 'https://api.coinmarketcap.com/v2/ticker';
+  getCryptocurrenciesUrl = 'https://api.coinmarketcap.com/v2/ticker/';
 
   constructor(private _http: HttpClient) {}
 
   getCryptocurrencies(): Observable<any> {
-      const params = new HttpParams().set('start', '1').set('limit', '5').set('sort', 'rank');
+      const params = new HttpParams().set('limit', '5').set('sort', 'rank').set('start', '1');
       return this._http.get(this.getCryptocurrenciesUrl, {params: params});
   }
 
