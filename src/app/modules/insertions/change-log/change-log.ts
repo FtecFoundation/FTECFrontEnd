cd4e0b28501d385b330d99aaba44a07c221bdf38
Julia <arikamedoshika@gmail.com>
2018-06-15 21:21:45 +0300

    change log updated

--- src/app/modules/insertions/change-log/change-log.ts
+++ src/app/modules/insertions/change-log/change-log.ts
@@ -1,5 +1,17 @@
 export const changeLog = [
     {
+        'date': '15/06/2018',
+        'changes': [
+            'We finished the work on endpoints connected with Arbitrage Module',
+            'Upload of user picture from the server is added. Possibilities to upload, edit, ' +
+            'and delete user picture will be added as work on the user settings page progresses',
+            'There was a refactoring of the code of methods implemented before',
+            'Videos and texts with description were added to the pages of modules which are still being developed',
+            'We solved some problems with sending requests linked with proxy configuration',
+            'We added functionality to buttons on the Social Assistant page'
+        ]
+    },
+    {
         'date': '14/06/2018',
         'changes': [
             'Popup on not implemented features',
