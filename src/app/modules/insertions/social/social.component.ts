1d16df4e69a2d595cfeb8ab0b0edd7a947d5545f
Julia <arikamedoshika@gmail.com>
2018-06-15 12:34:36 +0300

    test proxy server

--- src/app/modules/insertions/social/social.component.ts
+++ src/app/modules/insertions/social/social.component.ts
@@ -1,33 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ShowModalService} from '../../not-active/show-modal.service';
 
 @Component({
-  selector: 'app-social',
-  templateUrl: './social.component.html',
-  styleUrls: ['../insertions.scss']
+    selector: 'app-social',
+    templateUrl: './social.component.html',
+    styleUrls: ['../insertions.scss']
 })
 export class SocialComponent implements OnInit {
 
-  dictionary: string[] = ['hardfork', 'partnership', 'airdrop', 'coinburn'];
-  newWord: string = '';
+    dictionary: string[] = ['hardfork', 'partnership', 'airdrop', 'coinburn'];
+    newWord = '';
 
-  constructor(private _showModalService: ShowModalService) {
-  }
+    constructor(private _showModalService: ShowModalService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
     showModal() {
         this._showModalService.showModal = true;
     }
 
     deleteWord(word: string) {
-    this.dictionary = this.dictionary.filter(wordInDict => wordInDict !== word);
-    console.log(this.dictionary);
+        this.dictionary = this.dictionary.filter(wordInDict => wordInDict !== word);
     }
 
     addWord() {
-    if(this.dictionary.indexOf(this.newWord) === -1) this.dictionary.push(this.newWord);
+        if (this.dictionary.indexOf(this.newWord) === -1) {
+            this.dictionary.push(this.newWord);
+        }
     }
 
 }
