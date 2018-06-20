d6dc8eed93ffc43dc9cb209fad4b4f6483417ea2
User <user@MacBook-Pro-User-3.local>
2018-06-20 13:16:13 +0300

    small changes

--- src/app/modules/insertions/settings/settings.component.ts
+++ src/app/modules/insertions/settings/settings.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-social',
   templateUrl: './settings.component.html',
-  styleUrls: ['../insertions.scss']
+  styleUrls: ['../insertions.scss', './settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
 
