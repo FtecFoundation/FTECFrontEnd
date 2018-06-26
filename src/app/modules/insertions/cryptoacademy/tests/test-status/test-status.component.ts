678ef03776dd5cbb4e4b79f392e17c661a5cfb56
User <user@MacBook-Pro-User-3.local>
2018-06-26 16:42:30 +0300

    insertions failure

--- src/app/modules/insertions/cryptoacademy/tests/test-status/test-status.component.ts
+++ src/app/modules/insertions/cryptoacademy/tests/test-status/test-status.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test-status',
   templateUrl: './test-status.component.html',
-  styleUrls: ['../insertions.scss', '../../cryptoacademy.component.scss']
+  styleUrls: ['../../../insertions.scss', '../../cryptoacademy.component.scss']
 })
 export class TestStatusComponent implements OnInit {
 
