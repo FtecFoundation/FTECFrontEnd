c92ef1eb6422b9bb429e8412bd247296d80fa5dc
User <user@MacBook-Pro-User-3.local>
2018-06-26 15:30:36 +0300

    tests layot

--- src/app/modules/insertions/cryptoacademy/tests/all-tests/all-tests.component.ts
+++ src/app/modules/insertions/cryptoacademy/tests/all-tests/all-tests.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-all-tests',
   templateUrl: './all-tests.component.html',
-  styleUrls: ['../../../insertions.scss', '../../cryptoacademy.component.scss']
+  styleUrls: ['../../../insertions.scss', '../../cryptoacademy.component.scss', '../tests.component.scss']
 })
 export class AllTestsComponent implements OnInit {
 
--- src/app/modules/insertions/cryptoacademy/tests/test-question/test-question.component.ts
+++ src/app/modules/insertions/cryptoacademy/tests/test-question/test-question.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test-blockchain',
   templateUrl: './test-question.component.html',
-  styleUrls: ['../../../insertions.scss', '../../cryptoacademy.component.scss']
+  styleUrls: ['../../../insertions.scss', '../../cryptoacademy.component.scss', '../tests.component.scss']
 })
 export class TestQuestionComponent implements OnInit {
 
