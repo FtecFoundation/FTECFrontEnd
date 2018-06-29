4d07b077ba449a6d1b576c42157deb129a5e45f3
User <user@MacBook-Pro-User-3.local>
2018-06-29 18:08:51 +0300

    everything to master

--- src/app/modules/insertions/faq/available-faq-left.ts
+++ src/app/modules/insertions/faq/available-faq-left.ts
@@ -1,20 +1,20 @@
 export const availableFaqLeft = [
   {
     'question': 'What is FTEC?',
-    'answer': 'An ecosystem of intelligent services and neural networks for conducting ' + 
+    'answer': 'An ecosystem of intelligent services and neural networks for conducting ' +
     'effective trading activities on cryptocurrency markets.',
     'chosen': false
   },
   {
     'question': 'What is Token Sale and pre-sale?',
-    'answer': 'Token sale is a way of crowdfunding using cryptocurrencies. Pre-sale is a ' + 
+    'answer': 'Token sale is a way of crowdfunding using cryptocurrencies. Pre-sale is a ' +
     'fundraising stage preceding the main round of Token Sale. Pre-sale tokens are usually sold cheaper.',
     'chosen': false
   },
   {
     'question': 'What is the pre-sale start date and how may I take part in it?',
-    'answer': 'The pre-sale event starts on April 24,2018, 14: 00 (GMT+ 00). The pre-sale ' + 
-    'will run for 28 days. After the pre-sale start, the tokens will be available for ' + 
+    'answer': 'The pre-sale event starts on April 24,2018, 14: 00 (GMT+ 00). The pre-sale ' +
+    'will run for 28 days. After the pre-sale start, the tokens will be available for ' +
     'purchase at https: //www.ftec.io/. Click "Buy tokens" and follow the simple instructions.',
     'chosen': false
   },
@@ -27,14 +27,14 @@ export const availableFaqLeft = [
   {
     'question': 'What are the objectives of FTEC?',
     'answer': 'The main goal of FTEC team is to build a unique trading ecosystem with a complete toolkit for working on ' +
-    'cryptocurrency markets. It is also important to form a broad community that will share the ' + 
+    'cryptocurrency markets. It is also important to form a broad community that will share the ' +
     'values of the project and actively support the developers.',
     'chosen': false
   },
   {
     'question': 'What approach to cryptomarket research do you use in the FTEC development?',
     'answer': 'Members of our team use a wide technology stack and research approaches to study the ' +
-    'latest trends in the field of cryptocurrency. In particular, FTEC applies methods of ' + 
+    'latest trends in the field of cryptocurrency. In particular, FTEC applies methods of ' +
     'machine learning, construction of ' +
     'neural networks, behavioral analysis.',
     'chosen': false
@@ -42,7 +42,7 @@ export const availableFaqLeft = [
   {
     'question': 'How can I make sure that the platform development will continue after the Token Sale?',
     'answer': 'The most important argument is that we already have the working projects and we are well-reputed.' +
-     'The FTEC team is a group of traders who have been working in this field for more than 5 ' + 
+     'The FTEC team is a group of traders who have been working in this field for more than 5 ' +
      'years. We have long thought about the implementation of ' +
      'such a platform and, first of all, are interested in this.',
     'chosen': false
