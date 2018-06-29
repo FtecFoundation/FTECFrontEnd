3bb72f94c40eaf140ae7089a022bffdd6275af5a
User <user@MacBook-Pro-User-3.local>
2018-06-29 10:35:09 +0300

    adaptation on desktop

--- src/app/modules/insertions/desktop-app/desktop-app.component.ts
+++ src/app/modules/insertions/desktop-app/desktop-app.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-social',
   templateUrl: './desktop-app.component.html',
-  styleUrls: ['../insertions.scss']
+  styleUrls: ['../insertions.scss', './desktop-app.component.scss']
 })
 export class DesktopAppComponent implements OnInit {
 
