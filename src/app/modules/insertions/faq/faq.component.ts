921bce57b1c40881639bcad7279be397c79e659a
User <user@MacBook-Pro-User-3.local>
2018-06-29 18:07:49 +0300

    push to master

--- src/app/modules/insertions/faq/faq.component.ts
+++ src/app/modules/insertions/faq/faq.component.ts
@@ -1,6 +1,8 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
-import {ImageService} from '../../../core/services/image.service';
-import {ShowModalService } from '../../not-active/show-modal.service';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { ImageService } from '../../../core/services/image.service';
+import { ShowModalService } from '../../not-active/show-modal.service';
+import { availableFaqLeft } from './available-faq-left';
+import { availableFaqRight } from './available-faq-right';
 
 @Component({
   selector: 'app-faq',
@@ -8,11 +10,15 @@ import {ShowModalService } from '../../not-active/show-modal.service';
   styleUrls: ['../insertions.scss', './faq.component.scss']
 })
 export class FaqComponent implements OnInit {
-  @ViewChild('uploader') inputImage: ElementRef;
-    private imageSrc = '';
-
+  show = true;
+  faqsLeft = availableFaqLeft;
+  faqsRight = availableFaqRight;
 
   ngOnInit() {
+  }
+
+  answer(faq: any) {
+    faq.chosen = !faq.chosen;
   }
 
 
