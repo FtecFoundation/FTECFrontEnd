21b0d7ad4718dcbf94ec31b11da3021eb4a403f1
User <user@MacBook-Pro-User-3.local>
2018-07-03 17:03:24 +0300

    first price notification push

--- src/app/modules/insertions/price-notification/price-notification.component.ts
+++ src/app/modules/insertions/price-notification/price-notification.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-social',
   templateUrl: './price-notification.component.html',
-  styleUrls: ['../insertions.scss']
+  styleUrls: ['../insertions.scss', './price-notification.component.scss']
 })
 export class PriceNotificationComponent implements OnInit {
 
