bb6c87e644d949bfff3f0e2df345fdd11b18b24a
User <user@MacBook-Pro-User-3.local>
2018-07-04 16:15:10 +0300

    problems on master

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -16,7 +16,7 @@ import {LoaderComponent } from './loader/loader.component';
 import {LoaderService} from './loader/loader.service';
 import {LanguageService} from './services/language.service';
 import {CryptoacademyService} from './services/cryptoacademy.service';
-
+import {CaptchaService} from './services/captcha.service';
 
 @NgModule({
     imports: [
@@ -38,7 +38,8 @@ import {CryptoacademyService} from './services/cryptoacademy.service';
         SocialService,
         LoaderService,
         LanguageService,
-        CryptoacademyService
+        CryptoacademyService,
+        CaptchaService
     ]
 })
 export class CoreModule {
