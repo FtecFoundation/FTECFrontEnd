5f359f0c0766ae7f70fbecd83ebddacad09fd48f
User <user@MacBook-Pro-User-3.local>
2018-07-06 13:06:19 +0300

    volume first

--- src/app/modules/insertions/trp/trp.component.ts
+++ src/app/modules/insertions/trp/trp.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { trpItems } from './trp';
 
 
-
 @Component({
   selector: 'app-social',
   templateUrl: './trp.component.html',
@@ -11,7 +10,6 @@ import { trpItems } from './trp';
 export class TrpComponent implements OnInit {
 
   trp = trpItems;
-  time = ["All time", "New", "Old", "Rating"]
 
   constructor() {
   }
--- src/app/modules/insertions/volume-analyzer/volume-analyzer.component.ts
+++ src/app/modules/insertions/volume-analyzer/volume-analyzer.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-social',
   templateUrl: './volume-analyzer.component.html',
-  styleUrls: ['../insertions.scss']
+  styleUrls: ['../insertions.scss', '../behavioral-analyzer/behavioral-analyzer.component.scss', '../social/social.component.scss', "../my-exchanges/my-exchanges.component.scss", '../price-notification/price-notification.component.scss', "../order-manager/order-manager.component.scss", "../global-price-analyzer/global-price-analyzer.component.scss"]
 })
 export class VolumeAnalyzerComponent implements OnInit {
 
