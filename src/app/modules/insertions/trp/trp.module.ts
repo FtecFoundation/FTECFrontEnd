4c76478df76ad0fb03a70f5a91fdf2b87640916a
User <user@MacBook-Pro-User-3.local>
2018-07-09 11:24:44 +0300

    volume done

--- src/app/modules/insertions/trp/trp.module.ts
+++ src/app/modules/insertions/trp/trp.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {TrpRoutingModule} from './trp-routing.module';
 import {TrpComponent} from './trp.component';
-import { SharedModule } from '../../../shared/shared.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule,
       TrpRoutingModule
   ],
   declarations: [TrpComponent]
--- src/app/modules/insertions/volume-analyzer/volume-analyzer.component.ts
+++ src/app/modules/insertions/volume-analyzer/volume-analyzer.component.ts
@@ -1,16 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { volumeAnalyzerItems } from './volume-analyzer';
+import { ShowModalService } from '../../not-active/show-modal.service';
+
 
 @Component({
   selector: 'app-social',
   templateUrl: './volume-analyzer.component.html',
-  styleUrls: ['../insertions.scss', '../behavioral-analyzer/behavioral-analyzer.component.scss', '../social/social.component.scss', "../my-exchanges/my-exchanges.component.scss", '../price-notification/price-notification.component.scss', "../order-manager/order-manager.component.scss", "../global-price-analyzer/global-price-analyzer.component.scss"]
+  styleUrls: ['../insertions.scss', '../behavioral-analyzer/behavioral-analyzer.component.scss', '../social/social.component.scss', "../my-exchanges/my-exchanges.component.scss", '../price-notification/price-notification.component.scss', "../order-manager/order-manager.component.scss", "../global-price-analyzer/global-price-analyzer.component.scss", "./volume-analyzer.component.scss"]
 })
 export class VolumeAnalyzerComponent implements OnInit {
 
-  constructor() {
+  items = volumeAnalyzerItems;
+
+  constructor(public _showModalService: ShowModalService) {
   }
 
   ngOnInit() {
+
+  }
+  
+  showModal() {
+    this._showModalService.showModal = true;
   }
 
 }
