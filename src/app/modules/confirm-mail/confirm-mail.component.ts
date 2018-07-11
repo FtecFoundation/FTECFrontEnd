1a8bce0ac77c91df175a8f9f9246820748db9c8e
julia <julia@MacBook.local>
2018-07-11 13:11:42 +0300

    messages

--- src/app/modules/confirm-mail/confirm-mail.component.ts
+++ src/app/modules/confirm-mail/confirm-mail.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import {ShowModalService} from '../not-active/show-modal.service';
-import { errorMessages } from '../../core/services/errors.service';
+
+export const messages: any = {
+    default: 'Your email has not been confirmed yet. Please confirm email or your account will be deleted ' +
+    'in two days after registration.',
+    successful: 'Your confirmation has been resent',
+    error: 'Something went wrong'
+};
 
 @Component({
   selector: 'app-confirm-mail',
   templateUrl: './confirm-mail.component.html',
   styleUrls: ['./confirm-mail.component.scss']
 })
+
 export class ConfirmMailComponent implements OnInit {
 
-  // showModal = false;
-  messages: any = {
-    default: "Your email has not been confirmed yet. Please confirm email or your account will be deleted " +
-     "in two days after registration.",
-    successful: "Your confirmation has been resent",
-    error: "Something went wrong"
-  }
-
-message: string = 
+  message: string = messages.default;
 
   constructor(private _showModalService: ShowModalService) { }
 
--- src/app/modules/insertions/cryptoacademy/tests/test-status/test-status.service.ts
+++ src/app/modules/insertions/cryptoacademy/tests/test-status/test-status.service.ts
@@ -25,7 +25,6 @@ export class TestStatusService {
                 }
             }
         }
-
     }
 
     getPassedAmount(history: TestHistory, test: number) {
