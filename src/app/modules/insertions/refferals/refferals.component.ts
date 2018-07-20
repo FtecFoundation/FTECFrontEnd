de74a6ffd05a05ffbeefe72f8b6e4b1f9834ecee
User <user@MacBook-Pro-User-3.local>
2018-07-20 16:12:18 +0300

    refferals tab

--- src/app/modules/insertions/refferals/refferals.component.ts
+++ src/app/modules/insertions/refferals/refferals.component.ts
@@ -10,11 +10,34 @@ export class RefferalsComponent implements OnInit {
   @ViewChild('uploader') inputImage: ElementRef;
     private imageSrc = '';
 
+  tabLvl1 = true;
+  tabLvl2 = false;
+  tabLvl3 = false;
+
   showPopupModal = false;
 
 
   ngOnInit() {
   }
+
+  showTab1() {
+    this.tabLvl2= false;
+    this.tabLvl3= false;
+    this.tabLvl1= true;
+  }
+
+  showTab2() {
+    this.tabLvl1= false;
+    this.tabLvl3= false;
+    this.tabLvl2= true;
+  }
+
+  showTab3() {
+    this.tabLvl1= false;
+    this.tabLvl2= false;
+    this.tabLvl3= true;
+  }
+
 
   showPopup() {
     this.showPopupModal = true;
