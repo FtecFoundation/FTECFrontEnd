2143c64c6e9ba1c24ed67a459c2aa12eba4c12d7
User <user@MacBook-Pro-User-3.local>
2018-07-24 10:12:56 +0300

    refactor styles end

--- src/app/modules/account/referrals/referrals.component.ts
+++ src/app/modules/account/referrals/referrals.component.ts
@@ -5,7 +5,7 @@ import {ShowModalService } from '../../not-active/show-modal.service';
 @Component({
   selector: 'app-referrals',
   templateUrl: './referrals.component.html',
-  styleUrls: [ '../../../../assets/styles/common.scss', './referrals.component.scss']
+  styleUrls: ['./referrals.component.scss']
 })
 export class ReferralsComponent implements OnInit {
   @ViewChild('uploader') inputImage: ElementRef;
