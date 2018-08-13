58681d274808d5bff6fc4ea66252376826c30533
User <user@MacBook-Pro-User-3.local>
2018-08-13 17:54:58 +0300

    logs

--- src/app/modules/insertions/price-notification/price-notification.module.ts
+++ src/app/modules/insertions/price-notification/price-notification.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PriceNotificationRoutingModule } from './price-notification-routing.module';
 import { PriceNotificationComponent } from './price-notification.component';
 import { SharedModule } from '../../../shared/shared.module';
-import { SliderComponent } from '../../slider/slider.component';
+
 
 
 @NgModule({
