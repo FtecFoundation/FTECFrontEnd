85c2c7653a68dbcdb5bc7bdaefce3a305bdf67c8
Yuliia Podpruhina <yuliia@MacBook-Pro-Yuliia.local>
2018-08-30 17:55:47 +0300

    notifications

--- src/app/core/notify/notify.component.ts
+++ src/app/core/notify/notify.component.ts
@@ -5,7 +5,23 @@ import {animate, animateChild, query, state, style, transition, trigger} from "@
 @Component({
     selector: 'app-notify',
     templateUrl: './notify.component.html',
-    styleUrls: ['./notify.component.scss']
+    styleUrls: ['./notify.component.scss'],
+    animations: [
+        trigger('notification', [
+            transition('void => *', [
+                style({marginRight: '-100%', opacity: 0}),
+                animate('500ms ease-out')
+            ]),
+            transition('* => void', [
+                animate('700ms ease-out', style({marginRight: '-100%', opacity: 0}))
+            ])
+        ]),
+        trigger('container', [
+            transition(':enter, :leave', [
+                query('@*', animateChild())
+            ])
+        ])
+    ]
 })
 export class NotifyComponent implements OnInit {
 
