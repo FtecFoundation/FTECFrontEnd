ddcc2f743e4459eecc674dbeb8e2c3abc25611ec
Andr1iSemenov <mr.andrii.semenov@gmail.com>
2018-12-10 18:35:29 +0200

    Removed prepared template for today chagnelog

--- src/app/modules/change-log/change-log.ts
+++ src/app/modules/change-log/change-log.ts
@@ -1,14 +1,5 @@
 export const changeLog = [
     {
-        'date': '10/12/2018',
-        'changes': [
-            'Button performance of Telegram assistant that checks whether the Telegram account was connected is corrected.',
-            'Pairs with USD/USDT currencies were added to Price analyzer.',
-            'Ability to hide the result from Global Price Analyzer makes user experience better.',
-            'Error that prevented user from adding new orders in Order Manager was corrected.'
-        ]
-    },
-    {
         'date': '06/12/2018',
         'changes': [
             'Button performance of Telegram assistant that checks whether the Telegram account was connected is corrected.',
