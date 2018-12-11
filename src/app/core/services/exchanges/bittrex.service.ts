6ce9b606d93cc6d2a2fe1ab1a033628ec44ce4be
Andr1iSemenov <mr.andrii.semenov@gmail.com>
2018-12-11 19:52:06 +0200

    One more try to resolve connection to inner server

--- src/app/core/services/exchanges/bittrex.service.ts
+++ src/app/core/services/exchanges/bittrex.service.ts
@@ -11,7 +11,7 @@ import {catchError} from "rxjs/operators";
 
 @Injectable()
 export class BittrexService implements ExchangeService {
-    baseUrl: string = 'http://46.101.51.140:8090/bittrex/';
+    baseUrl: string = 'https://ftec.network:8090/bittrex/';
     apiUrls = {
         getPairs: 'getmarkets',
         getPrice: 'getticker'
